cf4d4f340e4f54ff052bc43df75ad4926b44b973
Nikolay Averin <rizzzon116@mail.ru>
2024-01-29 21:19:26 +0400

    add home_work_3

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src.queue import *
+
+
+class TestQueue(unittest.TestCase):
+    def test_str(self):
+        queue = Queue()
+        self.assertEquals(str(queue), "")
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        self.assertEquals(str(queue), "data1\ndata2")
+
+    def test_enqueue(self):
+        queue = Queue()
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+        self.assertEquals(queue.head.data, 'data1')
+        self.assertEquals(queue.head.next_node.data, 'data2')
+        self.assertEquals(queue.tail.data, 'data3')
+        self.assertEquals(queue.tail.next_node, None)
